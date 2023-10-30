2bba00bfbf60a6131157d8cb13f088c512146fd0
Matheus <meduardo.valentim@gmail.com>
2023-10-30 18:53:59 -0300

    fix: correct secrets

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-mz!y_+1km^n3uhr%w(wv@bi#4#$p)58@2=ki=2^)cfzvj2*p32"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
